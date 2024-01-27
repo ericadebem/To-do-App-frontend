2df43ea19181178a2b6a8a47d5c03f726ec7dc90
Erica de Bem <ericadebemm@gmail.com>
2024-01-27 21:40:17 +0100

    Refactor TaskContext and TasksPage components

--- src/context/TaskContext.tsx
+++ src/context/TaskContext.tsx
@@ -1,9 +1,26 @@
-import React , { createContext, useContext, ReactNode} from "react";
+import React, { createContext, useContext, ReactNode } from "react";
+import { TasksPage } from "../pages/TasksPage";
+import { AddTaskPage } from "../pages/AddTasksPage";
 
-interface Task {
-    title: string;
-    data: Date;
-    axtion: string;
-    periodicity: string;
-
+interface ITask {
+  title: string;
+  date: number;
+  action: string;
+  periodicity: number;
 }
+
+interface ITaskProps {
+  data: ITask;
+}
+
+export const TaskContext = ({ data }: ITaskProps) => {
+  const { title, date, action, periodicity } = data;
+  return (
+    <div>
+      <p>{title}</p>
+      <p>{date}</p>
+      <p>{action}</p>
+      <p>{periodicity}</p>
+    </div>
+  );
+};
--- src/pages/TasksPage.tsx
+++ src/pages/TasksPage.tsx
@@ -1,18 +1,74 @@
 import { useState, useEffect } from "react";
 import { FetchData } from "../util/FetchData";
+import axios, { AxiosError, AxiosResponse } from "axios";
+import { TaskContext } from "../context/TaskContext";
+import { AddTasksPage } from "../pages/AddTasksPage"
+
+interface ITask {
+  title: string;
+  date: number;
+  action: string;
+  periodicity: number;
+}
+export const SendData = async (endpoint: string, data: ITask) => {
+  try {
+    await axios.post(`http://localhost:6060/app/${endpoint}`, data);
+  } catch (error: AxiosError) {
+    console.error(error);
+    throw new Error(error);
+  }
+};
 
 export const TasksPage = () => {
+  const [formData, setFormData] = useState({
+    title: "",
+    date: 0,
+    action: "",
+    periodicity: 50,
+  });
   return (
     <div>
       <h1>TODO APP</h1>
       <form>
-        <input type="text" placeholder="NEW TASK" name="newTask" id="newTask" />
-        <input type="number" placeholder="DATE" name="date" id="date" />
-        <input type="text" placeholder="ACTION" name="action" id="action" />
-        <input type="number" placeholder="PERIODICITY" name="periodicity" id="periodicity" />
+        <input
+          type="text"
+          placeholder="NEW TASK"
+          name="newTask"
+          id="newTask"
+          onChange={(e) =>
+            setFormData({ ...formData, title: String(e.target.value) })
+          }
+        />
+        <input
+          type="number"
+          placeholder="DATE"
+          name="date"
+          id="date"
+          onChange={(e) =>
+            setFormData({ ...formData, data: Number(e.target.value) })
+          }
+        />
+        <input
+          type="text"
+          placeholder="ACTION"
+          name="action"
+          id="action"
+          onChange={(e) =>
+            setFormData({ ...formData, action: String(e.target.value) })
+          }
+        />
+        <input
+          type="number"
+          placeholder="PERIODICITY"
+          name="periodicity"
+          id="periodicity"
+          onChange={(e) =>
+            setFormData({ ...formData, periodicity: Number(e.target.value) })
+          }
+        />
 
-       <button>ADD</button>
-       <button>CANCEL</button>
+        <button onClick={() => SendData(AddTasksPage, formData)}>ADD</button>
+        <button onClick={() => SendData(TasksPage, formData)}>CANCEL</button>
       </form>
     </div>
   );
