6ee03e6ad24f7b815d990e0e9f125719f424bede
Erica de Bem <ericadebemm@gmail.com>
2024-01-30 11:07:12 +0100

    Refactor TaskModal component and update main.scss

--- src/components/TaskModal.tsx
+++ src/components/TaskModal.tsx
@@ -12,26 +12,30 @@ export const TaskModal = ({ task }: ITaskProps) => {
 
   return (
     <div className="modal">
-      <p>{title}</p>
-      <p>|</p>
-      <p>{dateString}</p>
-      <button
-        className="Test"
-        onClick={async () => {
-          await DeleteData("task", task._id);
-          window.location.reload();
-        }}
-      >
-        <i className="far fa-trash-alt"></i>
-      </button>
-      <button
-        onClick={async () => {
-          await UpdateData("task", task._id);
-          window.location.reload();
-        }}
-      >
-        <i className="far fa-edit"></i>
-      </button>
+      <div>
+        <p>{title}</p>
+        <p>|</p>
+        <p>{dateString}</p>
+      </div>
+      <div>
+        <button
+          className="Test"
+          onClick={async () => {
+            await DeleteData("task", task._id);
+            window.location.reload();
+          }}
+        >
+          <i className="far fa-trash-alt"></i>
+        </button>
+        <button
+          onClick={async () => {
+            await UpdateData("task", task._id);
+            window.location.reload();
+          }}
+        >
+          <i className="far fa-edit"></i>
+        </button>
+      </div>
     </div>
   );
 };
